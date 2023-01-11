b1ff12cd393fc3107c4bc861cbacc38b14220570
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-01-11 20:08:39 +0900

    :sparkles: :: splash 추가

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Splash from './src/components/splash';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <>
+      <Splash/>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
