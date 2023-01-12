036d5ddd615f28bb0a13a0e45f89d66369283e91
parkdaxun <parkde0207@gmail.com>
2023-01-12 09:27:07 +0900

    [ADD]main screen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Main from "./src/screens/mainscreen";
+import Sleep from "./src/screens/sleepscreen";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+              <Stack.Screen name="Main" component={Main} />
+              <Stack.Screen name="Sleep" component={Sleep} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/loginscreen.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Image, Button, SafeAreaView, StyleSheet, TextInput, View, Alert, TouchableOpacity, onPress, Text, Dimensions } from 'react-native';
+
+const loginscreen = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.card}>
+
+                <Image style={styles.img} source={require('../images/logo.png')}/>
+
+                <TextInput style={styles.input} placeholder="이메일을 입력하세요" />
+                <TextInput style={styles.input} placeholder="비밀번호를 입력하세요" />
+
+                <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={onPress}
+                    >
+                        <Text style={styles.buttonText}>로그인</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={onPress}
+                    >
+                        <Text style={styles.buttonText}>회원가입</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7198FF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    img: {
+        marginBottom: 50,
+    },
+
+    button: {
+        alignItems: "center",
+        backgroundColor: "white",
+        width: 140,
+        padding: 10,
+        borderRadius: 10,
+        margin: 9,
+        marginTop: 50,
+
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 15,
+        letterSpacing: 0.5,
+        color: '#7198FF'
+    },
+
+    input: {
+        borderRadius: 10,
+        backgroundColor: 'white',
+        fontSize: 18,
+        padding: 10,
+        width: 300,
+        marginBottom: 20,
+    },
+
+    card: {
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+});
+
+export default loginscreen;
